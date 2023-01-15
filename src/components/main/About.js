ccd77499e90ac6b495e0cdfc76ce8940394df594
sobek123 <65244789+sobek123@users.noreply.github.com>
2023-01-15 17:29:27 +0100

    Update About.js

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -10,34 +10,34 @@ export function About(){
         <hr ></hr>
         <div className="containter">
             <span style={{fontSize: 18,textAlign:'center'}}><p>SobRent to obecnie najbardziej popularna wypożyczalnia samochodowa w województwie podlaskim według magazynu opinie.pl.
-            W swojej ofercie mamy ponad pięćdziesiąt samochodów z różnych kategorii, takich jak: sportowe, dostawcze, ekskluzywne, ekonomiczne, suvy, komfortowe.
+            W swojej ofercie mamy ponad 100 samochodów z różnych kategorii, takich jak: sportowe, dostawcze, ekskluzywne, ekonomiczne, suvy, komfortowe.
             Jesteśmy partnerami i sponosrami różnego rodzaju wydarzeń kulturalnych, społecznych, sportowych, a także aktywnie bierzemy udział w akcjach charytatywnych na rzecz pomocy potrzebującym.</p></span>
         
 
  
         <div className="grid-container" style={{float: 'left', width:'100%'}}>
             <div className="grid-container" style={{float: 'left', width:'50%',textAlign:'center'}}>
-                <img src={require('./cars/wyscigi-samochodowe-1.webp')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginLeft:40,marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
+                <img src={require('./cars/wyscigi-samochodowe-1.webp')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
                 <br></br>
-                <span style={{marginTop:320}}>Nasze samochody regularnie biorą udział w różnych wyścigach charytatywnych, podczas których zbierane są pieniądze na wiele zbiórek organizowanych przez fundacje!!</span>
+                <span style={{marginTop:320,marginLeft:-250}}>Nasze samochody regularnie biorą udział w różnych wyścigach charytatywnych, podczas których zbierane są pieniądze na wiele zbiórek organizowanych przez fundacje!!</span>
             </div>
             <div className="grid-container" style={{float: 'right', width:'50%',textAlign:'center'}}>
-                <img src={require('./cars/basketball-team-4609620_960_720.jpg')} alt="Drużyna koszykówki" width={500} height={300} style={{marginLeft:50,marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
+                <img src={require('./cars/basketball-team-4609620_960_720.jpg')} alt="Drużyna koszykówki" width={500} height={300} style={{marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
                 <br></br>
-                <span style={{marginTop:320}}>Jesteśmy jednym ze sponsorów oraz partnerów lokalnej drużyny koszykówki, która osiąga wielkie sukcesy na szczeblu rozgrywek wojewódzkich oraz ogólnopolskich.</span>
+                <span style={{marginTop:320,marginLeft:-250}}>Jesteśmy jednym ze sponsorów oraz partnerów lokalnej drużyny koszykówki, która osiąga wielkie sukcesy na szczeblu rozgrywek wojewódzkich oraz ogólnopolskich.</span>
             </div>
         </div>
         <br></br>
         <div className="grid-container" style={{float: 'right', width:'100%'}}>
             <div className="grid-container" style={{float: 'left', width:'50%',textAlign:'center'}}>
-                <img src={require('./cars/13-09318132d4c0478d49ce91b335e19.jpg')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginLeft:40,marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
+                <img src={require('./cars/13-09318132d4c0478d49ce91b335e19.jpg')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
                 <br></br>
-                <span style={{marginTop:320}}>Bierzemy udział w wielu festiwalach oraz wystawach, aby zaprezentować uczestnikom część naszej floty pojazdów i przy tym zachęcić jak najwięcej osób do skorzystania z naszej oferty.</span>
+                <span style={{marginTop:320,marginLeft:-250}}>Bierzemy udział w wielu festiwalach oraz wystawach, aby zaprezentować uczestnikom część naszej floty pojazdów i przy tym zachęcić jak najwięcej osób do skorzystania z naszej oferty.</span>
             </div>
             <div className="grid-container" style={{float: 'right', width:'50%',textAlign:'center'}}>
-                <img src={require('./cars/szlachetna_paczka_720.png')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginLeft:50,marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
+                <img src={require('./cars/szlachetna_paczka_720.png')} alt="Wyścigi samochodowe" width={500} height={300} style={{marginRight:'auto',textAlign:'center', borderRadius: 15}}></img>
                 <br></br>
-                <span style={{marginTop:320}}>Systematycznie wspieramy materialnie rodziny i osoby, które znajdują się w trudnej sytucji życiowej przekazując pieniądze na rzecz Szlachetnej Paczki.</span>
+                <span style={{marginTop:320,marginLeft:-250}}>Systematycznie wspieramy materialnie rodziny i osoby, które znajdują się w trudnej sytucji życiowej przekazując pieniądze na rzecz Szlachetnej Paczki.</span>
             </div>
         </div>
       <br></br>
